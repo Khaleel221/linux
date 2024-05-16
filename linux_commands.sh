06c33db4d79bf5aacd7e311f2c3948eb0030c6ce
Khaleel <khaleelshaik711@gmail.com>
2024-05-16 12:31:47 +0530

    frontend expense project

--- linux_commands.sh
+++ linux_commands.sh
@@ -4,6 +4,4 @@ ls -A #to check hidden files
 pwd #to check present working directory
 cd / # to change from one directory
 cd .. #refer to parent directory
-cd # simple cd command redirect to home directory
-touch file.xt #to create files
-mkdir file #to create directy
+cd #
